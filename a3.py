64f1b187b530ef457ee4ac108b52f27b36998b3b
Sam <hartogssam@gmail.com>
2023-09-29 10:02:00 +0200

    added variation 3 and 4

--- a3.py
+++ a3.py
@@ -76,4 +76,27 @@ J2 = cv2.filter2D(J1, -1, box_kernel(2))
 H2 = cv2.filter2D(H1, -1, box_kernel(2))
 
 # Character variation 3 (noise)
+noise = np.random.normal(0, 0.5, S.shape)
+S3 = S1 + noise
+S3 = np.clip(S3,0,1)
 
+noise = np.random.normal(0, 0.5, J.shape)
+J3 = J1 + noise
+J3 = np.clip(J3,0,1)
+
+noise = np.random.normal(0, 0.5, H.shape)
+H3 = H1 + noise
+H3 = np.clip(H3,0,1)
+
+# Character variation 4 (blur + noise)
+noise = np.random.normal(0, 0.5, S.shape)
+S4 = S2 + noise
+S4 = np.clip(S4,0,1)
+
+noise = np.random.normal(0, 0.5, J.shape)
+J4 = J2 + noise
+J4 = np.clip(J4,0,1)
+
+noise = np.random.normal(0, 0.5, H.shape)
+H4 = H2 + noise
+H4 = np.clip(H4,0,1)
