cf73a71fe55b52785368e865086399ee3a9542f9
josh <joshwbrook01@gmail.com>
2023-10-04 17:39:45 +0200

    create function to check letters and plot. also update NN

--- a3.py
+++ a3.py
@@ -146,23 +146,60 @@ plt.savefig("letter_correlation_matrix.png")
 plt.show()
 
 
+def normalise(mat):
+    return (10 * (mat / np.linalg.norm(mat))).round(2)
+
+
+# Create normalised combination matrices
+SN = normalise(S1 + S2 + S3 + S4)
+JN = normalise(J1 + J2 + J3 + J4)
+HN = normalise(H1 + H2 + H3 + H4)
+
 # Recognition matrix
-NN1 = np.array([S, J, H])
+NN1 = np.array([SN, JN, HN])
 
-# Plot chosen letter
-test = S4
-plt.matshow(np.reshape(np.array(test), [5,5]), cmap="Blues")
+
+def recognise(nn, letter, name):
+
+    plt.matshow(np.reshape(np.array(letter), [5, 5]), cmap="Greens")
+    plt.show()
+
+    # clarify 0s with negative weights
+    for ii in range(len(letter)):
+        if letter[ii] == 0:
+            letter[ii] = letter[ii] - 0.1
+
+    mat = np.matmul(nn, letter)
+    normal = normalise(mat)
+    amax = np.argmax(normal)
+
+    fig, ax = plt.subplots(figsize=(6, 4))
+    ax.bar([0, 1, 2], normal, color=["#74baf7", "#fad56e", "#de6ef0"], width=0.6)
+    ax.set_xticks(np.arange(0, 3, 1))
+    ax.set_xticklabels(["S", "J", "H"], fontsize=20, fontfamily='serif')
+
+    plt.title(name, fontsize=25, pad=15, fontfamily='serif')
+    plt.savefig("plots/letter_recognition_" + name + ".png")
+    plt.show()
+
+    if amax == 0:
+        return "S"
+    elif amax == 1:
+        return "J"
+    elif amax == 2:
+        return "H"
+
+
+# Checks and plots all 12 letters
+for i, l in enumerate([S1, S2, S3, S4, J1, J2, J3, J4, H1, H2, H3, H4]):
+    name = "S" + str(i + 1)
+    if i >= 4:
+        name = "J" + str(i - 3)
+    if i >= 8:
+        name = "H" + str(i - 7)
+    print(i + 1, recognise(NN1, l, name))
+
+
+# Plot blurring matrix
+plt.matshow(blurring_matrix, cmap="Greens")
 plt.show()
-
-# Test NN
-print(np.matmul(NN1, test))
-print(np.argmax(np.matmul(NN1, test)))
-plt.bar([0,1,2], np.matmul(NN1, test), tick_label=["S", "J", "H"], color=["#74baf7", "#fad56e", "#de6ef0"])
-plt.show()
-
-
-"""
-print(str(np.dot(S, S)) + '\t' + str(np.dot(S, J)) + '\t' + str(np.dot(S, H)))
-print(str(np.dot(J, S)) + '\t' + str(np.dot(J, J)) + '\t' + str(np.dot(J, H)))
-print(str(np.dot(H, S)) + '\t' + str(np.dot(H, J)) + '\t' + str(np.dot(H, H)))
-"""
